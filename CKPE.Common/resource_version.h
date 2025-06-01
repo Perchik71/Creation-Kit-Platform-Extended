4727fb6ad3f9b8b70a7aea4d3da1cd245a77f3b8
Alexey <vlad.shabalin.1992@mail.ru>
2025-06-01 15:41:07 +0300

    [CKPE]

--- CKPE.Common/resource_version.h
+++ CKPE.Common/resource_version.h
@@ -4,7 +4,7 @@
 
 // Copyright © 2023-2024 aka perchik71. All rights reserved.
 // Contacts: <email:timencevaleksej@gmail.com>
-// License: https://www.gnu.org/licenses/gpl-3.0.html
+// License: https://www.gnu.org/licenses/lgpl-3.0.html
 
 // Следующие стандартные значения для новых объектов
 // 
