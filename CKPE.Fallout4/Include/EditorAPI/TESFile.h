43da0838983bcf132d88559f453ab4a8bcf8f570
Alexey <vlad.shabalin.1992@mail.ru>
2025-10-31 21:29:03 +0300

    [CKPE] utf-8 src code convert

--- CKPE.Fallout4/Include/EditorAPI/TESFile.h
+++ CKPE.Fallout4/Include/EditorAPI/TESFile.h
@@ -1,4 +1,4 @@
-// Copyright © 2022-2025 aka perchik71. All rights reserved.
+// Copyright Â© 2022-2025 aka perchik71. All rights reserved.
 // Contacts: <email:timencevaleksej@gmail.com>
 // License: https://www.gnu.org/licenses/lgpl-3.0.html
 
