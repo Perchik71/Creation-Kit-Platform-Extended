5df9823cf53865d8be485e96f498eff2becae255
Mathias Dejerud Rådstam <68670981+FlenarnDev@users.noreply.github.com>
2026-08-21 13:06:50 +0200

    Update CKPE.Fallout4.VersionLists.h

--- CKPE.Fallout4/Include/CKPE.Fallout4.VersionLists.h
+++ CKPE.Fallout4/Include/CKPE.Fallout4.VersionLists.h
@@ -39,6 +39,7 @@ namespace CKPE
 			[[nodiscard]] static std::wstring GetEditorVersionByString() noexcept(true);
 			[[nodiscard]] static std::wstring GetExternalResourcePackageFileName() noexcept(true);
 			[[nodiscard]] static EDITOR_EXECUTABLE_TYPE GetEditorVersion() noexcept(true);
+			[[nodiscard]] static std::wstring GetAddressLibraryRelativePath() noexcept(true);
 		};
 	}
 }
