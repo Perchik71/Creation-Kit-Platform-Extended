4009755618be7d175f20d49433bb068c67791c3c
Alexey <vlad.shabalin.1992@mail.ru>
2026-09-18 10:25:38 +0300

    feat: redo precombined patch, patch installing only if sets pc mode

--- CKPE.Fallout4/Src/Patches/CKPE.Fallout4.Patch.PreCombined.cpp
+++ CKPE.Fallout4/Src/Patches/CKPE.Fallout4.Patch.PreCombined.cpp
@@ -15,6 +15,24 @@ namespace CKPE
 	{
 		namespace Patch
 		{
+			namespace PreGenCombined
+			{
+				enum class Mode
+				{
+					kGenerateForPC = 0,
+					kGenerateForPS4 = 2,
+				};
+
+				static Mode GenerateMode = Mode::kGenerateForPC;
+				static std::uint32_t* PreGenCombinedMode_orig = nullptr;
+
+				static void SetPreGenCombinedMode() noexcept(true)
+				{
+					if (PreGenCombinedMode_orig)
+						*PreGenCombinedMode_orig = std::to_underlying(GenerateMode);
+				}
+			};
+
 			PreCombined::PreCombined() : Common::Patch()
 			{
 				SetName("PreCombined");
@@ -47,44 +65,47 @@ namespace CKPE
 
 			bool PreCombined::DoActive(Common::RelocatorDB::PatchDB* db) noexcept(true)
 			{
-				// This of option control the output of the havok collision information generated during the precombine process, which is stuck at mode 1 unpatched.
-				// This will not magically make your plugin the same format. - BenRierimanu
-				// 0 - 64bit havok little endian[PC or XB1, default setting with CKPE and recommended for PRP and related patches]
-				// 1 - 64bit havok big endian[PS4, default setting unpatched. Untested and not recommended unless you can somehow get the files on that platform]
-				// 2 - 32bit havok little endian[PC or XB1, default setting when using the user interface generation commands as the XB1 was originally a 32bit platform at one point, will still work, but obsolete]
-
 				using namespace Common;
 
-				auto precomb_flag_1i = static_cast<std::int32_t>(_READ_OPTION_INT("PreCombined", "nGenerationVersion", 2));
-				auto precomb_flag_2i = static_cast<std::int32_t>(_READ_OPTION_INT("PreCombined", "nGenerationVersionSCOL", 1));
-				auto precomb_flag_3i = static_cast<std::int32_t>(_READ_OPTION_INT("PreCombined", "nGenerationVersionMesh32bit", 1));
-				auto precomb_flag_1b = static_cast<std::int8_t>(precomb_flag_1i);
-				auto precomb_flag_2b = static_cast<std::int8_t>(precomb_flag_2i);
-				auto precomb_flag_3b = static_cast<std::int8_t>(precomb_flag_3i);
-
-				Relocation(ID{ 677726, 1377514 }, 0x13B).Write(std::addressof(precomb_flag_1i), 4);
-
-				if (VersionLists::GetEditorVersion() == VersionLists::EDITOR_FALLOUT_C4_1_10_162_0)
+				std::int32_t precomb_flag = std::to_underlying(PreGenCombined::Mode::kGenerateForPC);
+				auto precomb_mode = _READ_OPTION_INT("PreCombined", "bGenerateForPC", true);
+				if (!precomb_mode)
 				{
-					auto rel1 = Relocation(ID(632758));
-					rel1.Write<0x38>(std::addressof(precomb_flag_2b), 1);
-					rel1.Write<0x44>(std::addressof(precomb_flag_2b), 1);
-					rel1.Write<0x54>(std::addressof(precomb_flag_2b), 1);
-					rel1.Write<0x67>(std::addressof(precomb_flag_2b), 1);
-					rel1.Write<0x6E>(std::addressof(precomb_flag_2i), 4);
-					rel1.Write<0x7D>(std::addressof(precomb_flag_2b), 1);
+					PreGenCombined::GenerateMode = PreGenCombined::Mode::kGenerateForPS4;
+					precomb_flag = std::to_underlying(PreGenCombined::GenerateMode);
+				}
+				auto precomb_flag_b = static_cast<std::int8_t>(precomb_flag);
 
-					auto rel2 = Relocation(ID(415101));
-					rel2.Write<0xC7>(std::addressof(precomb_flag_3i), 4);
-					rel2.Write<0xD6>(std::addressof(precomb_flag_3b), 1);
+				if (PreGenCombined::GenerateMode == PreGenCombined::Mode::kGenerateForPS4)
+					// For PS4 no needs patch
+					return true;
+
+				Relocation(ID{ 63416, 1696443 }).WriteJump(&PreGenCombined::SetPreGenCombinedMode);
+				PreGenCombined::PreGenCombinedMode_orig = Relocation<std::uint32_t*>(ID{ 975208, 1623094 }).Get();
+
+				Relocation(ID{ 677726, 1377514 }, 0x13B).Write(std::addressof(precomb_flag), 4);
+
+				if (VersionLists::GetEditorVersion() != VersionLists::EDITOR_FALLOUT_C4_1_10_162_0)
+				{
+					Relocation(ID(1462644), 0x29).Write(std::addressof(precomb_flag), 4);
+
+					auto rel1 = Relocation(ID(1990514));
+					rel1.Write<0x116>(std::addressof(precomb_flag), 4);
+					rel1.Write<0x125>(std::addressof(precomb_flag_b), 1);
 				}
 				else
 				{
-					Relocation(ID(1462644), 0x29).Write(std::addressof(precomb_flag_2i), 4);
+					auto rel1 = Relocation(ID(632758));
+					rel1.Write<0x38>(std::addressof(precomb_flag_b), 1);
+					rel1.Write<0x44>(std::addressof(precomb_flag_b), 1);
+					rel1.Write<0x54>(std::addressof(precomb_flag_b), 1);
+					rel1.Write<0x67>(std::addressof(precomb_flag_b), 1);
+					rel1.Write<0x6E>(std::addressof(precomb_flag), 4);
+					rel1.Write<0x7D>(std::addressof(precomb_flag_b), 1);
 
-					auto rel1 = Relocation(ID(1990514));
-					rel1.Write<0x116>(std::addressof(precomb_flag_3i), 4);
-					rel1.Write<0x125>(std::addressof(precomb_flag_3b), 1);
+					auto rel2 = Relocation(ID(415101));
+					rel2.Write<0xC7>(std::addressof(precomb_flag), 4);
+					rel2.Write<0xD6>(std::addressof(precomb_flag_b), 1);
 				}
 
 				return true;
